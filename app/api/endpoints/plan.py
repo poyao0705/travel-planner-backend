b9061c4174417e756c2816db076b3926f7dfb8d4
poyaohuang <poyaohg0705@gmail.com>
2026-03-20 12:52:14 +1100

    chore: black format

--- app/api/endpoints/plan.py
+++ app/api/endpoints/plan.py
@@ -5,33 +5,31 @@ from app.api.core.log import get_logger
 router = APIRouter()
 logger = get_logger(__name__)
 
+
 @router.get("")
 def get_plan():
     # req = await request.json()
     logger.info("Received request")
 
     return {
-  "trip_id": "uuid",
-  "destination": "Tokyo",
-  "days": 3,
-  "itinerary": [
-    {
-      "day": 1,
-      "date": "2024-06-01",
-      "items": [
-        {
-          "id": "poi_1",
-          "title": "Shibuya Crossing",
-          "description": "Famous crossing",
-          "location": {
-            "lat": 35.6595,
-            "lng": 139.7005
-          },
-          "start_time": "09:00",
-          "end_time": "11:00",
-          "type": "attraction"
-        }
-      ]
+        "trip_id": "uuid",
+        "destination": "Tokyo",
+        "days": 3,
+        "itinerary": [
+            {
+                "day": 1,
+                "date": "2024-06-01",
+                "items": [
+                    {
+                        "id": "poi_1",
+                        "title": "Shibuya Crossing",
+                        "description": "Famous crossing",
+                        "location": {"lat": 35.6595, "lng": 139.7005},
+                        "start_time": "09:00",
+                        "end_time": "11:00",
+                        "type": "attraction",
+                    }
+                ],
+            }
+        ],
     }
-  ]
-}
--- app/main.py
+++ app/main.py
@@ -5,8 +5,10 @@ from app.api.core.log import setup_logging
 app = FastAPI()
 setup_logging(level="INFO")
 
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
 
 app.include_router(v1, prefix="/api/v1")
